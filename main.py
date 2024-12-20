79d68fdeec43eb9fbfd5cf1127c59c75d9c3edc2
m4rsm4ps <stonedwelder@gmail.com>
2024-12-20 19:32:26 +0200

    added ability to turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from constants import *    
+from player import Player
 import pygame
 
 
@@ -10,13 +11,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    playr = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="Black")
+        playr.update(dt)
+        playr.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
